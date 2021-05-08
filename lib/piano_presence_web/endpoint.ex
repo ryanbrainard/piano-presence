6593a7d61e5941abc41a7bf19da5d168e5024eae
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2021-05-08 09:43:17 +0900

    heroku prod config

--- lib/piano_presence_web/endpoint.ex
+++ lib/piano_presence_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule PianoPresenceWeb.Endpoint do
   ]
 
   socket "/socket", PianoPresenceWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
